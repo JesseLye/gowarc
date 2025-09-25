a43cb878ab05be9b9ada648a95b4728f1a6130b0
Jesse Lye <jjlye6@outlook.com>
2025-09-25 15:18:48 +1000

    Updated utils to reference forked repository

--- utils.go
+++ utils.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/internetarchive/gowarc/pkg/spooledtempfile"
+	"github.com/JesseLye/gowarc/pkg/spooledtempfile"
 	"github.com/klauspost/compress/zstd"
 )
 
